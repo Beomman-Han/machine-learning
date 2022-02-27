febd0ff50d7d1f13c92d88958f1ebd7c925f17e4
Beomman-Han <qjaaks6378@gmail.com>
2022-02-28 01:34:17 +0900

    Arrange directory and split brute_force.py to each functions

--- algorithm_paradigm/brute_force/closest_pair.py
+++ algorithm_paradigm/brute_force/closest_pair.py
@@ -1,49 +1,6 @@
-from typing import List, Tuple
-
-def max_product(
-    left_cards: List[int],
-    right_cards: List[int]
-    ) -> int or None:
-    """Calculate max product of two int
-    from left_cards list and right_cards list.
-    If either left_cards or right_cards has no card,
-    it returns None for max_product.
-    
-    Time Complexity
-    ---------------
-    O(n * m)
-        n : length of left_cards
-        m : length of right_cards
-    
-    Parameters
-    ----------
-    left_cards : List[int]
-        left int cards list
-    right_cards : List[int]
-        right int cards list
-
-    Returns
-    -------
-    int
-        max product from left_cards and right_cards
-    """
-    
-    if len(left_cards) == 0 or len(right_cards) == 0:
-        return None
-    
-    ## initial setting
-    max_product = left_cards[0] * right_cards[0]
-    for left in left_cards:
-        for right in right_cards:
-            product = left * right
-            if product > max_product:
-                max_product = product
-        
-    return max_product
-
+from typing import Tuple, List
 from math import sqrt
 
-# 두 매장의 직선 거리를 계산해 주는 함수
 def distance(
     store1: Tuple[int],
     store2: Tuple[int]
@@ -64,7 +21,6 @@ def distance(
     """
     return sqrt((store1[0] - store2[0]) ** 2 + (store1[1] - store2[1]) ** 2)
 
-# Find the closest pair of 2 stores
 def closest_pair(
     coordinates: List[Tuple[int]]
     ) -> List[Tuple[int]] or List:
@@ -106,13 +62,9 @@ def closest_pair(
     
     return [*pairs[0]]
 
+
 if __name__ == '__main__':
-    
-    ## test for max_product
-    # print(max_product([1, 6, 5], [4, 2, 3]))
-    # print(max_product([1, -9, 3, 4], [2, 8, 3, 1]))
-    # print(max_product([-1, -7, 3], [-4, 3, 6]))
-    
+        
     # 테스트
     test_coordinates = [(2, 3), (12, 30), (40, 50), (5, 1), (12, 10), (3, 4)]
     print(closest_pair(test_coordinates))
