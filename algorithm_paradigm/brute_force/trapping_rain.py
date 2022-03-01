ec33ff3308bdc63d0029e010cb8fd4a7fd0906f4
Beomman-Han <qjaaks6378@gmail.com>
2022-03-01 17:07:02 +0900

    Add another efficient algo trapping_rain_v2

--- algorithm_paradigm/brute_force/trapping_rain.py
+++ algorithm_paradigm/brute_force/trapping_rain.py
@@ -1,5 +1,54 @@
 from typing import List
 
+def trapping_rain_v2(
+    buildings: List[int]
+    ) -> int:
+    """Calculate the amount of rain
+    trapped between buildings 
+    for reducing time complexity
+    compared to trapping_rain implementation.
+    
+    Ex) [3, 0, 0, 2, 0, 4] : the total number of '~' ('~' representing rain)
+
+         |             |
+    |    |        |~~~~|
+    |  | |   =>   |~~|~|
+    |  | |        |~~|~|
+    ------        ------
+                   ^
+                   
+    ** brute and force method
+    For all index of input list, calculate the amount of rain
+    Think about how to determine each amount of rain at an index
+
+    Time Complexity
+    ---------------
+    O(n^2)
+        n : length of buildings
+    
+    Parameters
+    ----------
+    buildings : List[int]
+        list of the height of buildings
+
+    Returns
+    -------
+    int
+        the amount of rain
+    """
+    
+    total_rain = 0
+    for i in range(1, len(buildings) - 1):
+        
+        left_max = max(buildings[:i])
+        right_max = max(buildings[i+1:])
+        
+        rain = min(left_max, right_max) - buildings[i]
+        if rain < 0:
+            rain = 0
+        total_rain += rain
+    return total_rain
+
 def trapping_rain(
     buildings: List[int]
     ) -> int:
@@ -50,5 +99,5 @@ def trapping_rain(
 if __name__ == '__main__':
 
     # 테스트
-    print(trapping_rain([3, 0, 0, 2, 0, 4]))  ## 4 + 4 + 1 + 1 = 10
-    print(trapping_rain([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))  ## 1 + 3 + 1 + 1
+    print(trapping_rain_v2([3, 0, 0, 2, 0, 4]))  ## 4 + 4 + 1 + 1 = 10
+    print(trapping_rain_v2([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))  ## 1 + 3 + 1 + 1
