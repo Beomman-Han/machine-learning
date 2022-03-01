89148eb0b7928a4911bd3d6de4e96eda4a3e656c
Beomman-Han <qjaaks6378@gmail.com>
2022-03-01 21:54:11 +0900

    Add merge_sort function

--- algorithm_paradigm/divide_conquer/merge_sort.py
+++ algorithm_paradigm/divide_conquer/merge_sort.py
@@ -1,18 +1,30 @@
 ## merge function implementation for merge sort
 
-def merge(list1: list, list2: list) -> list:
+from typing import List
+
+
+def merge(
+    list1 : List[int],
+    list2 : List[int]
+    ) -> List[int]:
+    
     """Merge from two sorted list to sorted list
 
+    Time Complexity
+    ---------------
+    O(n)
+        n : sum of the length of list1 and list2
+    
     Parameters
     ----------
-    list1 : _type_
+    list1 : List[int]
         sorted list 1
-    list2 : _type_
+    list2 : List[int]
         sorted list 2
     
     Returns
     -------
-    list
+    List[int]
         merged sorted list
     """
     
@@ -36,10 +48,49 @@ def merge(list1: list, list2: list) -> list:
 
     return merged
 
-# 테스트
-print(merge([1],[]))
-print(merge([],[1]))
-print(merge([2],[1]))
-print(merge([1, 2, 3, 4],[5, 6, 7, 8]))
-print(merge([5, 6, 7, 8],[1, 2, 3, 4]))
-print(merge([4, 7, 8, 9],[1, 3, 6, 10]))
+def merge_sort(
+    my_list: List[int]
+    ) -> List[int]:
+    """Merge sort by divide and conquer method
+
+    Time Complexity
+    ---------------
+    O(n*lg n)
+        n : length of input list
+        
+    Parameters
+    ----------
+    my_list : List[int]
+        input list
+
+    Returns
+    -------
+    List[int]
+        sorted list
+    """
+    
+    ## base case (conquer for subproblem)
+    if len(my_list) == 1:
+        return my_list[:]
+    
+    ## recursive case (divide)
+    left = merge_sort(my_list[:len(my_list) // 2])
+    right = merge_sort(my_list[len(my_list) // 2:])
+    
+    ## combine
+    return merge(left, right)
+
+if __name__ == '__main__':
+    # 테스트
+    print(merge_sort([1, 3, 5, 7, 9, 11, 13, 11]))
+    print(merge_sort([28, 13, 9, 30, 1, 48, 5, 7, 15]))
+    print(merge_sort([2, 5, 6, 7, 1, 2, 4, 7, 10, 11, 4, 15, 13, 1, 6, 4]))
+
+
+    # 테스트
+    print(merge([1],[]))
+    print(merge([],[1]))
+    print(merge([2],[1]))
+    print(merge([1, 2, 3, 4],[5, 6, 7, 8]))
+    print(merge([5, 6, 7, 8],[1, 2, 3, 4]))
+    print(merge([4, 7, 8, 9],[1, 3, 6, 10]))
