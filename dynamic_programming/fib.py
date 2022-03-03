4a48a695b702ebeaf6dd4c87f42a7abb0b482cb1
Beomman-Han <qjaaks6378@gmail.com>
2022-03-04 01:43:24 +0900

    Add 'fib_tab' func with tabulation method at fib.py

--- dynamic_programming/fib.py
+++ dynamic_programming/fib.py
@@ -1,5 +1,29 @@
-def fib_memo(n, cache):
-    # 코드를 작성하세요.
+from typing import Dict
+
+
+def fib_memo(
+    n : int,
+    cache : Dict[int, int]
+    ) -> int:
+    
+    """Calculate n th fibonacci number
+    with 'memoization' method (top-down approach).
+    
+
+    Parameters
+    ----------
+    n : int
+        n th of fibonacci
+    cache : Dict[int, int]
+        Storage saving already calculated fib number
+
+    Returns
+    -------
+    int
+        n th fib number
+    """
+    
+    ## if n th number is in cache, return it
     if n in cache:
         return cache[n]
     
@@ -15,8 +39,28 @@ def fib(n):
 
     return fib_memo(n, fib_cache)
 
-
 # 테스트
 print(fib(10))
 print(fib(50))
 print(fib(100))
+
+def fib_tab(n : int):
+    """Calculate n th fibonacci number 
+    with 'tabulation' method (bottom-up approach).
+
+    Parameters
+    ----------
+    n : int
+        n th of fibonacci
+    """
+    
+    table = {1 : 1, 2 : 1}
+    for i in range(3, n + 1):
+        table[i] = table[i - 1] + table[i - 2]        
+
+    return table[n]
+
+# 테스트
+print(fib_tab(10))
+print(fib_tab(56))
+print(fib_tab(132))
