bb7fb406c5b8e85bfed8a5f7c668456f7b03c899
Beomman-Han <qjaaks6378@gmail.com>
2022-03-04 02:50:16 +0900

    Create algorithm_paradigm/dynamic_programming/max_profit.py

---
+++ dynamic_programming/max_profit.py
@@ -0,0 +1,63 @@
+from typing import Dict, List
+
+
+def max_profit_memo(
+    price_list : List[int],
+    count : int,
+    cache : Dict[int, int]
+    ) -> int:
+    
+    """Calculate max profit of 'count' products.
+    'price_list' has price of some count of products.
+    
+    Ex) [0, 100, 400, 800] - 100 for 1 product, 400 for 2 product, ...
+    
+    Time Complexity
+    ---------------
+    O(n)
+        n : count
+    
+    Parameters
+    ----------
+    price_list : List[int]
+        Price table
+    count : int
+        # of products
+    cache : Dict[int, int]
+        Storage of max profits
+    
+    Returns
+    -------
+    int
+        Max profit of 'count' products
+    """
+
+    ## memoization
+    if count in cache:
+        return cache[count]
+
+    ## base case
+    if count == 1:
+        cache[count] = price_list[count]
+        return cache[count]
+        
+    ## recursive case
+    profits = []
+    if count < len(price_list):
+        profits.append(price_list[count])
+        
+    for i in range(1, count // 2 + 1):
+        profits.append(max_profit_memo(price_list, count - i, cache) + max_profit_memo(price_list, i, cache))
+    
+    return max(profits)
+    
+def max_profit(price_list, count):
+    max_profit_cache = {}
+
+    return max_profit_memo(price_list, count, max_profit_cache)
+
+
+# 테스트
+print(max_profit([0, 100, 400, 800, 900, 1000], 5))
+print(max_profit([0, 100, 400, 800, 900, 1000], 10))
+print(max_profit([0, 100, 400, 800, 900, 1000, 1400, 1600, 2100, 2200], 9))
