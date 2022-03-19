26676458b9179cbc1e60a7621f31c8531d34b273
Beomman-Han <qjaaks6378@gmail.com>
2022-03-20 03:54:10 +0900

    Make test_script.sh for test code and apply to main.yaml

---
+++ test_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## test codes in algorithm_paradigm/
+for py in `ls algorithm_paradigm/*/*.py`; do
+    python $py
+done
+
+## test codes in data_structure/
+for py in `ls data_structure/*.py`; do
+    python $py
+done
