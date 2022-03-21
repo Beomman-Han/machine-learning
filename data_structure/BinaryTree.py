3a09631f0a2854160ed5e1651dc92fbbd8ec309e
Beomman-Han <qjaaks6378@gmail.com>
2022-03-21 19:04:06 +0900

    Add heapify function at BinaryTree.py

--- data_structure/BinaryTree.py
+++ data_structure/BinaryTree.py
@@ -54,6 +54,68 @@ def traverse_postorder(node : Type['Node']):
 
     return
 
+def swap(
+    tree : list,
+    index_1 : int,
+    index_2 : int
+    ) -> None:
+    
+    """Swap elements at index1 and index2 in tree list
+
+    Parameters
+    ----------
+    tree : list
+        Input tree list
+    index_1 : int
+        Index1 for swapping
+    index_2 : int
+        Index2 for swapping
+    """
+    
+    tree[index_1], tree[index_2] = tree[index_2], tree[index_1]    
+    
+    return
+
+def heapify(
+    tree : list,
+    index : int,
+    tree_size : int
+    ) -> None:
+    
+    """Heapify node at index in tree
+
+    Parameters
+    ----------
+    tree : list
+        Input tree
+    index : int
+        Index of node to heapify
+    tree_size : int
+        Input tree size
+    """
+    
+    ## base case
+    left_child_index = 2 * index
+    right_child_index = 2 * index + 1
+    if not (0 < index < tree_size) or not (0 < left_child_index < tree_size):
+        return
+
+    ## recursive case
+    largest = index
+    if tree[largest] < tree[left_child_index]:
+        largest = left_child_index
+    
+    if 0 < right_child_index < tree_size and\
+    tree[largest] < tree[right_child_index]:
+        largest = right_child_index
+    
+    if largest != index:
+        swap(tree, largest, index)
+        heapify(tree, largest, tree_size)
+
+    return
+
+
 if __name__ == '__main__':
     
     ## init node instance
@@ -80,3 +142,8 @@ if __name__ == '__main__':
     traverse_preorder(root_node)
     print('## traverse postorder')
     traverse_postorder(root_node)
+    
+    ## test heapify function
+    tree = [None, 15, 5, 12, 14, 9, 10, 6, 2, 11, 1]  # heapify하려고 하는 완전 이진 트리
+    heapify(tree, 2, len(tree))  # 노드 2에 heapify 호출
+    print(tree) 
