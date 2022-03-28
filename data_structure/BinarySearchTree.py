2a9e8639278199cdc77a41165ea1720005415f81
Beomman-Han <qjaaks6378@gmail.com>
2022-03-28 23:57:33 +0900

    Create BinarySearchTree.py at 'data_structure/'

---
+++ data_structure/BinarySearchTree.py
@@ -0,0 +1,11 @@
+class Node:
+    """Node class for BinarySearchTree class"""
+    def __init__(self, data):
+        self.data = data
+        self.parent = None
+        self.left_child = None
+        self.right_child = None
+        return
+
+class BinarySearchTree:
+    ...
