f0c450c9c8ee946ecf07a4f98d278ed789506efa
Beomman-Han <qjaaks6378@gmail.com>
2022-04-21 00:50:19 +0900

    Add test lines for added methods of Maze class

--- algorithm/classic_problem/test_ch2_search.py
+++ algorithm/classic_problem/test_ch2_search.py
@@ -5,3 +5,8 @@ from ch2_search import *
 if __name__ == '__main__':
     maze : Maze = Maze()
     print(maze)
+    
+    print(maze.goal_test(MazeLocation(4, 5)))  ## False
+    print(maze.goal_test(MazeLocation(9, 9)))  ## True
+    
+    print(maze.successors(MazeLocation(0, 0)))
