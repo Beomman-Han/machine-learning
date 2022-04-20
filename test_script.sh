1f4372447ee27fb935d59eca01954dfadfc7849f
Beomman-Han <qjaaks6378@gmail.com>
2022-04-20 20:56:06 +0900

    Update test_script.sh to test algorithm/classic_problem/*py

--- test_script.sh
+++ test_script.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-## test codes in algorithm_paradigm/
+## test for algorithm/algorithm_paradigm
 for py in `ls algorithm/algorithm_paradigm/*/*.py`; do
     python $py
 done
 
-## test codes in data_structure/
+## test for algorithm/classic_problem
+for py in `ls algorithm/classic_problem/*py`; do
+    python $py
+done
+
+## test for data_structure
 for py in `ls data_structure/*.py`; do
     python $py
 done
