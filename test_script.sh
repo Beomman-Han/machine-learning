5be871dd8a3fc358f2a31595d7b25fa6b66ca643
Beomman-Han <qjaaks6378@gmail.com>
2022-05-22 22:09:01 +0900

    Update test script for CI

--- test_script.sh
+++ test_script.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-## test for algorithm/algorithm_paradigm
-for py in `ls algorithm/algorithm_paradigm/*/*.py`; do
-    python $py
-done
-
-## test for algorithm/classic_problem
-for py in `ls algorithm/classic_problem/*py`; do
-    python $py
-done
-
-## test for data_structure
-for py in `ls data_structure/*.py`; do
-    python $py
+## run all python codes at machine_learning
+for py in `ls machine_learning/*py`; do
+    python ${py}
 done
